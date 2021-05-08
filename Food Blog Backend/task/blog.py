c9984046b846e110aabd345f2bf4f959398eb67b
jolayemi_local <ajolayemi82@gmail.com>
2021-05-08 16:38:19 +0200

    Defined MealsTable class

--- Food Blog Backend/task/blog.py
+++ Food Blog Backend/task/blog.py
@@ -8,18 +8,20 @@ def cli_arguments():
     """ Handles command line arguments using argparse module. """
     parser = argparse.ArgumentParser(description='Accepts a single argument which is a database'
                                                  'name')
-    parser.add_argument('dd_name', type=str, help='Database name')
+    parser.add_argument('db_name', type=str, help='Database name')
     return parser.parse_args()
+
+
+db_name = cli_arguments().db_name
 
 
 class DatabaseCon:
     """ Creates database connection. """
 
-    def __init__(self, database_name: str):
+    def __init__(self, database_name: str = db_name):
         self.db = database_name
         self.cursor = None
         self.connection = None
-        self.create_con()
 
     def create_con(self):
         """ Creates database connection. """
@@ -35,6 +37,34 @@ class DatabaseCon:
                f'(connection, {self.connection}]'
 
 
+class MealsTable(DatabaseCon):
+    table_name = 'meals'
+
+    def __init__(self, meal_id: int, meal_name: str):
+        super().__init__()
+        self.create_con()
+        self.meal_id = meal_id
+        self.meal_name = meal_name
+
+        self.table_creator()
+
+    def populate_table(self):
+        """ Populates meals table. """
+        query = f'INSERT INTO meals (meal_id, meal_name) VALUES (' \
+                f'{self.meal_id}, {self.meal_name})'
+        self.cursor.execute(query)
+        self.connection.commit()
+
+    def table_creator(self):
+        """ Creates meals table. """
+        query = f'CREATE TABLE IF NOT EXISTS  {MealsTable.table_name} (' \
+                f'meals_id INT PRIMARY KEY,' \
+                f'meal_name VARCHAR(20) NOT NULL UNIQUE )'
+        self.cursor.execute(query)
+        self.connection.commit()
+
+
 if __name__ == '__main__':
-    a = DatabaseCon('test')
-    print(a.__repr__())
+    a = MealsTable(1, 'a')
+
+
